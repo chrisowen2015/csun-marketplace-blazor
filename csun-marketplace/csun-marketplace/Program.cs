0e3eaa03972d112f660abfdcacf2fde20cf38be3
chrisowen2015 <39311449+chrisowen2015@users.noreply.github.com>
2022-03-08 16:36:46 -0800

    Removed Default Stuff
    Removed the default template components that are only clutter

--- csun-marketplace/csun-marketplace/Program.cs
+++ csun-marketplace/csun-marketplace/Program.cs
@@ -1,4 +1,3 @@
-using csun_marketplace.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using MudBlazor.Services;
@@ -8,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddMudServices();
 
 var app = builder.Build();
