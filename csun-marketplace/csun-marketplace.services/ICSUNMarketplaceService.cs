b86a6250a8574822865d0272359eb64016174944
chrisowen2015 <39311449+chrisowen2015@users.noreply.github.com>
2022-03-10 14:48:03 -0800

    Recovered backend code
    Bruh

---
+++ csun-marketplace/csun-marketplace.services/ICSUNMarketplaceService.cs
@@ -0,0 +1,16 @@
+﻿using csun_marketplace.data;
+
+
+namespace csun_marketplace.services
+{
+    public interface ICSUNMarketplaceService
+    {
+        Product GetProduct(int productId);
+        List<Product> GetProductList();
+        List<Product> GetUsersProducts(int userId);
+        int UpdateProduct(Product pvm);
+        UserInformation GetUserInformation(int userId);
+        int UpdateUserInformation(UserInformation uvm);
+
+    }
+}
