6e6f2e1817bf8b3e2825427bed9b78a709e20e4f
arthursulian <arthursulian@gmail.com>
2022-03-15 19:49:12 -0700

    added Identity
    Login / Logout / Register

--- csun-marketplace/csun-marketplace/Program.cs
+++ csun-marketplace/csun-marketplace/Program.cs
@@ -4,6 +4,9 @@ using MudBlazor.Services;
 using csun_marketplace.services;
 using csun_marketplace.dbo;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using csun_marketplace.Data;
+using csun_marketplace.Areas.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,8 +15,11 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddMudServices();
 builder.Services.AddDbContextFactory<CSUNMarketplaceContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<csun_marketplaceContext>();builder.Services.AddDbContext<csun_marketplaceContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("csun_marketplaceContextConnection")));
 builder.Services.AddScoped<ICSUNMarketplaceService, CSUNMarketplaceService>();
+builder.Services.AddScoped<TokenProvider>();
 
 var app = builder.Build();
 
@@ -30,7 +36,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
