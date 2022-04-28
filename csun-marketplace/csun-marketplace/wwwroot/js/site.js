bbecf888a249074003301e8407a458999fe39e87
chrisowen2015 <39311449+chrisowen2015@users.noreply.github.com>
2022-04-27 17:55:25 -0700

    Completed initial PayPal SDK setup
    Buttons appear on checkout, will complete more later

---
+++ csun-marketplace/csun-marketplace/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+﻿
+
+function displayButtons(element) {
+    var style = {
+        layout: 'vertical',
+        color: 'blue',
+        shape: 'rect',
+        label: 'paypal'
+    }
+
+    var createOrder = {
+
+    }
+
+    paypal.Buttons({ style }).render(element);
+}
